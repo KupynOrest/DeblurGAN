1421a74e4b150b05139acb87a09f5b14d7e8302d
mykhalychnickolay <mykhalychnickolay>
2017-11-10 13:59:00 +0200

    added

--- motion_blur/blur_image.py
+++ motion_blur/blur_image.py
@@ -112,8 +112,9 @@ if __name__ == '__main__':
     folder_to_save = '/Users/mykolam/PycharmProjects/University/DeblurGAN2/blured'
     params = [0.01, 0.009, 0.008, 0.007, 0.005, 0.003]
     for path in os.listdir(folder):
+        print(path)
         trajectory = Trajectory(canvas=64, max_len=60, expl=np.random.choice(params)).fit()
         psf = PSF(canvas=64, trajectory=trajectory).fit()
         BlurImage(os.path.join(folder, path), PSFs=psf,
-                  path__to_save=folder_to_save, part=np.random.choice(range(4))).\
+                  path__to_save=folder_to_save, part=np.random.choice([1, 2, 3])).\
             blur_image(save=True)
