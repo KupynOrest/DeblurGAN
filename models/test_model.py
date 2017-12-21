e16d51e6288d8bf89dc69ad36fd68ffa3a0e2dca
keineahnung2345 <mimifasosofamire1123@gmail.com>
2017-12-21 11:59:26 +0800

    make it to run on cpu successfully
    to run on cpu, add "--gpu -1" in the end of the command "python test.py"

--- models/test_model.py
+++ models/test_model.py
@@ -27,7 +27,9 @@ class TestModel(BaseModel):
     def set_input(self, input):
         # we need to use single_dataset mode
         input_A = input['A']
-        self.input_A.resize_(input_A.size()).copy_(input_A)
+        temp = self.input_A.clone()
+        temp.resize_(input_A.size()).copy_(input_A)
+        self.input_A = temp
         self.image_paths = input['A_paths']
 
     def test(self):
